f8d50af11cb94e5be9eb09ec185b77966e6070c1
Santi <santiese2014@gmail.com>
2022-06-06 19:02:06 -0300

    agrego import os

--- tpfinal.py
+++ tpfinal.py
@@ -23,21 +23,14 @@ def consultar_cuentas():
         elif tipo_moneda == "2":
             print("Saldo disponible: 85.000 Pesos")
 
-<<<<<<< HEAD
+
     
-    
+import os #<---- Este módulo provee una manera versátil de usar funcionalidades dependientes del sistema operativo    
 clave_ingresada = 12345#int(input("Ingrese la clave: "))
 if (clave_ingresada==clave) == False:
-=======
-
-
-
-#import getpass
-clave_ingresada = 12345 #int(input("Ingrese la clave: "))
-if (clave_ingresada==CLAVE) == False:
->>>>>>> d3f122d1c772e1f7b00ddf445eac8b3984fb5ba6
     contador_intentos = 0
     while contador_intentos<3 and (clave_ingresada==CLAVE) == False:
+        os.system('cls') #<--- Limpia la pantalla (SOLO OS DE WINDOWS, PARA LINUX O MAC CAMBIAR cls por clear) 
         clave_ingresada = int(input("La clave ingresada es incorrecta, intente otra vez: "))
         contador_intentos = contador_intentos + 1
         if contador_intentos == 3 and (clave_ingresada==CLAVE) == False:
@@ -47,14 +40,14 @@ dni_ingresado = 12345678 #int(input("Ingrese el numero de dni: "))
 if (dni_ingresado==DNI) == False:
     print("El DNI ingresado es incorrecto")
     exit()
-<<<<<<< HEAD
+
 #print(" 1. Consultas   2. Retiros   3. Transferencias   4. Salir")
 #opcion_menu = int(input())
 #if opcion_menu == 1:
 tipo_moneda = int(input("Ingrese el tipo de moneda 1 para Soles y 2 para Pesos Argentinos "))
 #    consultar_cuentas()
 menu()
-=======
+
 print(" 1. Consultas   2. Retiros   3. Transferencias   4. Salir")
 
 opcion_menu = int(input())
@@ -63,4 +56,4 @@ if opcion_menu == 1:
 
     consultar_cuentas()
 
->>>>>>> d3f122d1c772e1f7b00ddf445eac8b3984fb5ba6
+
