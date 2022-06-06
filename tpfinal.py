e8b1927eab43b25e3e4d5e578ed67bb66785aa63
Joaquin Cremer <joaquincremer@gmail.com>
2022-06-06 18:19:56 -0300

    Se agregó funcion menu y se añadieron elementos a funcion consultar_uentas

--- tpfinal.py
+++ tpfinal.py
@@ -6,15 +6,25 @@
 clave = 12345
 dni = 12345678
 
+def menu():
+    print(" 1. Consultas   2. Retiros   3. Transferencias   4. Salir")
+    opcion_menu = int(input())
+    if opcion_menu == 1:
+        consultar_cuentas()
+    
+
 def consultar_cuentas():
     print("a - Posición Global")
     print("b - Movimientos")
     opcion_cuentas = input()
+    if opcion_cuentas == "a":
+        if tipo_moneda == 1:
+            print("Saldo displonible: 3.564 Soles")
+        elif tipo_moneda == "2":
+            print("Saldo disponible: 85.000 Pesos")
 
-        
-
-
-
+    
+    
 clave_ingresada = 12345#int(input("Ingrese la clave: "))
 if (clave_ingresada==clave) == False:
     contador_intentos = 0
@@ -28,20 +38,9 @@ dni_ingresado = 12345678 #int(input("Ingrese el numero de dni: "))
 if (dni_ingresado==dni) == False:
     print("El DNI ingresado es incorrecto")
     exit()
-print(" 1. Consultas   2. Retiros   3. Transferencias   4. Salir")
-
-opcion_menu = int(input())
-if opcion_menu == 1:
-    tipo_moneda = input("Ingrese el tipo de moneda: ")
-     
-    consultar_cuentas()
-
-else:
-    print("nati")
-    
-
-
-
-
-
-    
+#print(" 1. Consultas   2. Retiros   3. Transferencias   4. Salir")
+#opcion_menu = int(input())
+#if opcion_menu == 1:
+tipo_moneda = int(input("Ingrese el tipo de moneda 1 para Soles y 2 para Pesos Argentinos "))
+#    consultar_cuentas()
+menu()
