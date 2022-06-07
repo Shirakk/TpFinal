a86c460fca5981257ad2258b043f713c504ebc2f
Santi <santiese2014@gmail.com>
2022-06-06 21:33:12 -0300

    se mejoro el os.system y algunas cosas importantes

--- tpfinal.py
+++ tpfinal.py
@@ -24,27 +24,28 @@ def consultar_cuentas():
             print("Saldo disponible: 85.000 Pesos")
 
 
-    
-import os #<---- Este módulo provee una manera versátil de usar funcionalidades dependientes del sistema operativo    
-clave_ingresada = 12345#int(input("Ingrese la clave: "))
-if (clave_ingresada==clave) == False:
+
+import os #<---- Este módulo provee una manera versátil de usar funcionalidades dependientes del sistema operativo
+os.system('cls')  
+clave_ingresada = int(input("Ingrese su clave: ")) #12345
+if clave_ingresada != CLAVE:
     contador_intentos = 0
-    while contador_intentos<3 and (clave_ingresada==CLAVE) == False:
-        os.system('cls') #<--- Limpia la pantalla (SOLO OS DE WINDOWS, PARA LINUX O MAC CAMBIAR cls por clear) 
+    while contador_intentos<3 and clave_ingresada != CLAVE:
+        os.system('cls') #<--- Limpia la pantalla (SOLO OS DE WINDOWS, PARA LINUX O MAC CAMBIAR cls POR clear) 
         clave_ingresada = int(input("La clave ingresada es incorrecta, intente otra vez: "))
         contador_intentos = contador_intentos + 1
-        if contador_intentos == 3 and (clave_ingresada==CLAVE) == False:
+        if contador_intentos == 3 and clave_ingresada != CLAVE:
             print("ATENCIÓN: Muchos intentos fallidos, tarjeta retenida.")
             exit()
-dni_ingresado = 12345678 #int(input("Ingrese el numero de dni: "))
-if (dni_ingresado==DNI) == False:
+dni_ingresado = int(input("Ingrese su numero de DNI: ")) #12345678 
+if dni_ingresado != DNI:
     print("El DNI ingresado es incorrecto")
     exit()
 
 #print(" 1. Consultas   2. Retiros   3. Transferencias   4. Salir")
 #opcion_menu = int(input())
 #if opcion_menu == 1:
-tipo_moneda = int(input("Ingrese el tipo de moneda 1 para Soles y 2 para Pesos Argentinos "))
+tipo_moneda = int(input("Ingrese el tipo de moneda, presione 1 para Soles y 2 para Pesos Argentinos "))
 #    consultar_cuentas()
 menu()
 
